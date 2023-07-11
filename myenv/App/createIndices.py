045b4f57d535f1edf3c21bc12fcc53f4acf6959c
princedalsaniya <prince.d@ahduni.edu.in>
2023-07-11 13:43:05 +0530

    Added this : - Created functions for creating and storing index for each chapter file in docs folder. - Created index for each files and stored in folder.

--- myenv/App/createIndices.py
+++ myenv/App/createIndices.py
@@ -1,4 +1,8 @@
 from llama_index import download_loader, VectorStoreIndex
+from database import availableDatabase, get_params_from_labels, getParams
+from pathlib import Path
+from llama_hub.file.unstructured.base import UnstructuredReader
+
 
 def createIndexForAllFilesTogather():
   SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
@@ -6,3 +10,25 @@ def createIndexForAllFilesTogather():
   documents = loader.load_data()
   index = VectorStoreIndex.from_documents(documents)
   index.storage_context.persist("./combinedVectorIndex")
+
+
+def createIndexForFile(labels):
+  chapter_ids = get_params_from_labels(labels, availableDatabase, 'id')
+  for id in chapter_ids:
+    loader = UnstructuredReader()
+    documents = loader.load_data(file=Path(f'./Docs/{id}.pdf'))
+    index = VectorStoreIndex.from_documents(documents)
+    index.storage_context.persist(f"./vectorIndexDataStore/{id}_index")
+
+
+def createIndexForEachFile():
+  for subject in availableDatabase:
+    createIndexForFile(getParams(availableDatabase[subject], 'label'))
+
+
+def main():
+  createIndexForEachFile()
+
+
+if __name__ == '__main__':
+  main()
